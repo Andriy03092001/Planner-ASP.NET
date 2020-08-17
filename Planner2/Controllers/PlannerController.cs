49c5382cb33c1a54b5e4fedd7242fc3b86eb7ec0
Andriy03092001 <cuanid236316@gmail.com>
2020-08-17 19:20:17 +0300

    first

---
+++ Planner2/Controllers/PlannerController.cs
@@ -0,0 +1,36 @@
+﻿using Planner2.Entity;
+using Planner2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planner2.Controllers
+{
+    public class PlannerController : Controller
+    {
+        private readonly EFContext _context;
+
+        public PlannerController()
+        {
+            _context = new EFContext();
+        }
+
+        public ActionResult ListEvents()
+        {
+            List<EventViewModel> data = _context.Events.Select(t => new EventViewModel { 
+                 Id = t.Id,
+                 Date = t.Date,
+                 Description = t.Description,
+                 Image = t.Image,
+                 Title = t.Title
+            }).ToList();
+
+            return View(data);
+        }
+
+
+
+    }
+}
---
+++ Planner2/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Planner2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Planner2.Entity.EFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Planner2.Entity.EFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
